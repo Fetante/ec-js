765cabd89315f6b131ebca251f27ca3ba648f552
Fetante <andreas.carlssonmunkedal@gmail.com>
2024-01-02 17:36:40 +0100

    Switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
-import Home from './views/Home'
-import Contacts from './views/Contacts'
-import News from './views/News'
-import NewsDetails from './views/NewsDetails'
-import NotFound from './views/NotFound';
-import { ArticleProvider } from './contexts/ArticleContext';
+import Home from "./views/Home";
+import Contacts from "./views/Contacts";
+import News from "./views/News";
+import NewsDetails from "./views/NewsDetails";
+import NotFound from "./views/NotFound";
+import { ArticleProvider } from "./contexts/ArticleContext";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ArticleProvider>
         <Routes>
-          <Route path='/' element={ <Home/> } />
-          <Route path='/contacts' element={ <Contacts/> } />
-          <Route path='/news' element={ <News/> } />
-          <Route path='news/:id' element={ <NewsDetails/> } />          
-          <Route path='/*' element={ <NotFound/> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/news" element={<News />} />
+          <Route path="news/:id" element={<NewsDetails />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </ArticleProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
